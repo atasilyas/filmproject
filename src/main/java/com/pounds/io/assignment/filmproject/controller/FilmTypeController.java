9566fd78bd36792b192d38af857fd67ad7caf7c1
Ömer Taşdelen <omer@3pounds.io>
2021-01-16 21:37:39 +0300

    FilmTypeApi is  Completed

---
+++ src/main/java/com/pounds/io/assignment/filmproject/controller/FilmTypeController.java
@@ -0,0 +1,15 @@
+package com.pounds.io.assignment.filmproject.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:9090/")
+@RestController
+@RequestMapping("filmTypeApi")
+@RequiredArgsConstructor
+public class FilmTypeController {
+
+}
